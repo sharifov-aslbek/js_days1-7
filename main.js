9659b6addfabba6cce3e5eea193e6ae893a37dfe
Aslbek <sharifovaslbek5@gmail.com>
2024-01-26 15:26:46 +0500

    added on git hub

---
+++ main.js
@@ -0,0 +1,42 @@
+ let number = prompt('1 dan 7 gacha raqam kiriting !')
+
+ if (number == 1) {
+    console.log('Dushanba')
+ }
+
+ else if (number == 2) {
+    console.log('Seshanba')
+ }
+ else if (number == 3) {
+    console.log('Chorshanba')
+ }
+ else if (number == 4) {
+    console.log('Payshanba')
+ }
+ else if (number == 5) {
+    console.log('Juma')
+ }
+ else if (number == 6) {
+    console.log('Shanba')
+ }
+ else if (number == 7) {
+    console.log('Yakshanba')
+ }
+
+ else if (number == null) {
+    console.log('Siz chiqib ketdingiz')
+ }
+
+ else if (number == '') {
+    console.log('Siz hech narsa yozmadingiz')
+ }
+
+
+ else if (number > 7) {
+    console.log('7 raqamidan oshib ketdi !')
+ }
+
+ else if (number < 1) {
+    console.log('Eng kami 1 raqami !')
+ }
+
